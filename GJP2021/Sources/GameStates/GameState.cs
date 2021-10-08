0a946dde13cf8e638e433744bac5cc2fe746ebf7
Nikola Schrodinger <desuhansa@gmail.com>
2021-10-08 21:14:59 +0200

    Added gamestates

---
+++ GJP2021/Sources/GameStates/GameState.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace GJP2021.Sources.GameStates
+{
+    public interface IGameState
+    {
+        void Update(GameTime gameTime);
+        void Draw(GameTime gameTime);
+        void Initialize(Kolori game);
+    }
+    
+}
