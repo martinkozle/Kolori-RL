2e2727eeb155b7ea66656acd3113fac91ce92356
Nikola Schrodinger <desuhansa@gmail.com>
2021-10-08 17:27:16 +0200

    init

---
+++ GJP2021/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GJP2021
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
